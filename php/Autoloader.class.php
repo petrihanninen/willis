de51235c4a93eb55898742de8714e688a556eacd
Petri Hänninen <petri.hanninen@dingle.fi>
2018-08-31 15:00:56 +0300

    Add traits to autoloader

--- php/Autoloader.class.php
+++ php/Autoloader.class.php
@@ -9,16 +9,22 @@
 
 class Autoloader {
   public static function register() {
-    spl_autoload_register(function ($className) {
-      // Look into Class folder
-      $file = 'Class/';
-      // Recursive serach for namespaced classed
-      $file .= str_replace('\\', DIRECTORY_SEPARATOR, $className).'.php';
+    spl_autoload_register(function ($name) {
+      // Recursive search for..
+      // Classes
+      $file_class = 'Class/' . str_replace('\\', DIRECTORY_SEPARATOR, $name).'class.php';
+      // Traits
+      $file_trait = 'Traits/' . str_replace('\\', DIRECTORY_SEPARATOR, $name).'traits.php';
 
-      if (file_exists($file)) {
-        require $file;
+      if (file_exists($file_class)) {
+        require $file_class;
+        return true;
+      } elseif (file_exists($file_trait)) {
+        require $file_trait;
         return true;
       }
+
+      // Failed to load a class or trait
       return false;
     });
   }
